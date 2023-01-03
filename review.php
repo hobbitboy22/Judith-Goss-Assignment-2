2685cb9268ed79b5d478830467653351503a8b75
hobbitboy22 <hobbitboy22@gmail.com>
2023-01-03 15:15:19 +0000

    Added a new stylesheet

--- review.php
+++ review.php
@@ -2,6 +2,8 @@
 <?php session_start() ?>
 <?php require __DIR__ . "/components/review-form.php"; ?>
 
+<style><?php include "css/StyleSheet" ?></style>
+
 <?php
 
     if(isset($_SESSION['loggedIn']) == true){
@@ -10,7 +12,7 @@
     else{
         echo "Please log in to write a review"; 
         echo ('<form action = "./login.php">
-            <input type = "submit" value = "Login"/>
+            <input type = "submit" class = "button" id = "reviewbutton" value = "Login"/>
         </form>');
     }
 
