e95988fd31db100427253c1b155590c850cdfbd5
hobbitboy22 <hobbitboy22@gmail.com>
2023-01-05 11:03:36 +0000

    PHP to shopw table data in column (NOT FINISHED)

--- add-product.php
+++ add-product.php
@@ -1,5 +1,6 @@
 <?php require "./inc/header.php"; ?>
 <? session_start(); ?>
+<?php require "./inc/functions.php"; ?>
 <?php require "./components/add-product-form.php"; ?>
 
 <?php require "./inc/footer.php"; ?>
--- products.php
+++ products.php
@@ -1,5 +1,40 @@
 <?php require __DIR__ . "/inc/header.php"; ?>
-     
-   <h1>Products!</h1>
+<?php session_start(); ?>
+<?php require "./inc/functions.php"; ?>
+
+<style><?php include "css/StyleSheet" ?></style>
+
+<h1>Products!</h1>
+
+<?php
+
+   $products = $member = $controllers->products()->get_all_products(); ?>
+   
+<table>
+   <tr>
+   <?php foreach ($products as $product){ ?>
+      <td>
+         <div class="container text-center">
+            <div class="row align-items-start">
+
+               <div class="col-3" >
+                  <div class = "row">
+                     <p id = "productshow"><?= $product['name'] ?></p>
+                  </div>
+                  <div class = "row">
+                     <p id = "productshow"><?= $product['price'] ?></p>
+                  </div>
+                  <div class = "row">
+                     <p id = "productshow"><?= $product['description'] ?></p>
+                  </div>
+               </div>
+
+            </div>
+         </td>
+      </div>
+   <?php } ?>
+   </tr>
+</table>
+
 
 <?php require __DIR__ . "/inc/footer.php"; ?>
