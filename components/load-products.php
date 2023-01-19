beb71e307767aab1a2576ac678e403b6f03bb7d1
hobbitboy22 <hobbitboy22@gmail.com>
2023-01-19 12:42:46 +0000

    Completed review page and admin control

---
+++ components/load-products.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+require_once './inc/functions.php';
+
+if (isset($_POST["delete"]))
+{
+    $id = (int)$_POST['delete'];
+
+    $del_product = $controllers->products()->delete_product($id);
+}
+
+$products =$controllers->products()->get_all_products();
+
+foreach ($products as $product):
+?>
+    <div class="text-center col-4">
+        <div class="container py-5 h-75">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <a class="card product-link" style="width: 18rem;" href="./product.php?id=<?= $product['id'] ?>">
+                    <img src="<?= $product['image'] ?>" class="card-img-top" alt="image of <?= $product['name'] ?>">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $product['name'] ?></h5>
+                        <p class="card-text">£<?= $product['price'] ?></p>
+                        <form method="post" action="">
+                            <?php if (isset($_SESSION['user'])):
+                                if ($_SESSION['user']['accountType'] == 'admin'): ?>
+                                    <button name="delete" class="btn btn-danger" value="<?php echo $product['id']; ?>" type="submit">Delete product</button>
+                                <?php endif;
+                            endif; ?>
+                        </form>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+<?php 
+endforeach;
+?>
